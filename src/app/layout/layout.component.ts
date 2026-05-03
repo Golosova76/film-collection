6953653384c8226fc77fb60c95ad27700dc7dde0
Anna Golosova <golosova76@gmail.com>
2026-05-03 12:23:02 +0300

    add header, main and footer block

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, BreadcrumbsComponent, RouterOutlet],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
